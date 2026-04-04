2d47a90c9c1047b5caa05171d5d21b6c5b3caedc
Rafi <77590285+therafiniac@users.noreply.github.com>
2026-04-04 06:47:47 +0530

    feat(theme): add custom color theme
    * feat(theme): Add new color palate
    
    * chore: Add tw animate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col bg-black">
+    <div className="flex min-h-screen flex-col bg-white">
       <main className="flex-1">
         {/* Hero Section */}
         <section className="container mx-auto px-4 py-32">
           <div>
-            <h1>A better way to track your job application..</h1>
+            <h1 className="text-black">
+              A better way to track your job application..
+            </h1>
             <p className="text-gray-700">
               Capture, organize and manage your job search in one place.
             </p>
