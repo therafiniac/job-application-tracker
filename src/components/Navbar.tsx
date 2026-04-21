e4141b94b7a18a0cec46fb3bd36a7f0aebd3bc6a
therafiniac <therafiniac@gmail.com>
2026-04-21 23:45:29 +0530

    feat(auth): add sign out feature

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,19 @@ import { Briefcase } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
+import { getSession, signOut } from '@/lib/auth/auth';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 
-const Navbar = () => {
+export default async function Navbar() {
+  const session = await getSession();
   return (
     <nav className="border-b border-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
@@ -15,20 +26,58 @@ const Navbar = () => {
           Job Tracker
         </Link>
         <div className="flex items-center gap-4">
-          <Link href="/sign-in" className="text-gray-700 hover:text-black">
-            <Button variant="ghost" className="text-gray-700 hover:text-black">
-              Sign In
-            </Button>
-          </Link>
-          <Link href="/sign-up">
-            <Button className="bg-primary hover:bg-primary/90">
-              Start for free
-            </Button>
-          </Link>
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Dashboard
+                </Button>
+              </Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="ghost">
+                    <Avatar>
+                      <AvatarFallback className="bg-primary text-white">
+                        {session.user.name[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    <div>
+                      <p>{session.user.name}</p>
+                      <p>{session.user.email}</p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuItem onClick={async () => await signOut()}>
+                    Log Out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link href="/sign-in" className="text-gray-700 hover:text-black">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 hover:text-black"
+                >
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/sign-up">
+                <Button className="bg-primary hover:bg-primary/90">
+                  Start for free
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
   );
-};
-
-export default Navbar;
+}
